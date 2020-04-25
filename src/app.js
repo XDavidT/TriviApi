f6826ce6b43abbb5cb2534877990a55f836e4a9e
Ubuntu <ubuntu@ip-172-31-44-62.us-east-2.compute.internal>
2020-04-25 15:34:03 +0000

    Imorove to use cloudflare and delete custom CA

--- src/app.js
+++ src/app.js
@@ -12,10 +12,6 @@ app.use(cors())
 const modify_router = require('./routers/modify_db')
 const getters_router = require('./routers/get_db')
 
-const privateKey  = fs.readFileSync(__dirname+'/utilities/ssl/server.key', 'utf8');
-const certificate = fs.readFileSync(__dirname+'/utilities/ssl/server.crt', 'utf8');
-const credentials = {key: privateKey, cert: certificate}
-
 app.use('/modify',modify_router)
 app.use('/get',getters_router)
 
@@ -29,8 +25,6 @@ app.post('*',(req,res)=>{
     res.status(501).send("error")
 })
 
-const httpsServer = https.createServer(credentials,app)
-httpsServer.listen(3000,()=>{
-    console.log("Server is up in port 3000");
-    
+app.listen(80,()=>{
+    console.log("Server is up in port 80")
 })
