c54bf74130d61550f0e8e2e2b2f08598a283e5ef
David Tayar <xdavidt@gmail.com>
2020-04-28 16:47:51 +0300

    Clear code + improve error handle

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 require('dotenv').config()
-const https = require('https')
 const express = require('express')
-const fs = require('fs')
-const path = require('path')
 const bodyParser = require('body-parser')
-var cors = require('cors')
+const cors = require('cors')
+const config = require('config')
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
@@ -19,11 +17,11 @@ app.use('/get',getters_router)
 
 app.get('*',(req,res)=>{
     console.log("Someone try to GET.. look that:"+req.query);
-    res.status(501).send("error")
+    res.redirect(config.get('site'))
 })
 app.post('*',(req,res)=>{
     console.log("Someone try to POST.. look that:"+req.query);
-    res.status(501).send("error")
+    res.redirect(config.get('site'))
 })
 
 app.listen(80,()=>{
